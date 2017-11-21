34acb6b6ecac67f71a96a268d88b6bdf9a67b5c9
jasperdenduijf <31652548+jasperdenduijf@users.noreply.github.com>
2017-11-21 19:57:28 +0100

    Update svg.js

--- Homework/dataWeek4/svg.js
+++ Homework/dataWeek4/svg.js
@@ -1,9 +1,11 @@
-console.log("Hoi!")
+function finishLegend(){
+	
 d3.xml("test.svg", "image/svg+xml", function(error, xml) {
 	if (error) throw error;    
 	document.body.appendChild(xml.documentElement);   
 	FillColor();
 });
+}
 
 function FillColor(){
 	data = ["#ccece6","#99d8c9","#66c2a4","#41ae76","#238b45","#005824","ffffff"]
