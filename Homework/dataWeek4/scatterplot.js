9bae3c1d5c9249f86275a936ca6fd23bfa8aa5b5
jasperdenduijf <31652548+jasperdenduijf@users.noreply.github.com>
2017-11-22 11:01:53 +0100

    change back

--- Homework/dataWeek4/scatterplot.js
+++ Homework/dataWeek4/scatterplot.js
@@ -52,12 +52,26 @@ function scatterDraw(){
 	svg.append("g")
 		.attr("class", "x axis")
 		.attr("transform", "translate(0," + height + ")")
-		.call(xAxis);
+		.call(xAxis)
+	.append("text")
+		.attr("class", "label")
+		.attr("x", width)
+		.attr("y", -6)
+		.style("text-anchor", "end")
+		.text("Oosterlengte");
 
 	// create the y-ax
 	svg.append("g")
 		.attr("class", "y axis")
 		.call(yAxis)
+	.append("text")
+		.attr("class", "label")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end")
+		.text("Noorderbreedte");
+
 
 	// create the dots
 	svg.selectAll(".dot")
@@ -93,21 +107,6 @@ function scatterDraw(){
 		.attr("dy", ".35em")
 		.style("text-anchor", "end")
 		.text(function(d) { return d; });
-		
-	svg.append("text")
-		.attr("class", "label")
-		.attr("transform", "rotate(-90)")
-		.attr("y", 6)
-		.attr("dy", ".71em")
-		.style("text-anchor", "end")
-		.text("Noorderbreedte");
-		
-	svg.append("text")
-		.attr("class", "label")
-		.attr("x", width)
-		.attr("y", -6)
-		.style("text-anchor", "end")
-		.text("Oosterlengte");
 
 	d3.select("body")
 		.append("a")
